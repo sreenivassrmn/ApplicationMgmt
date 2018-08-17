781f780f3c7e7789ed916cf9a3f471277d07c293
Srmnsree23 <sreenivassrmn>
2018-08-17 21:55:28 +0530

    formatting the code
    Change-Id: I93a2d9b8773d1b258135d60aff715d5f0277675b
    Signed-off-by: Srmnsree23 <sreenivassrmn>

--- src/com/git/ex/second.java
+++ src/com/git/ex/second.java
@@ -1,7 +1,7 @@
 package com.git.ex;
 class second {
 public void m1() {
-        int a = 3,b=8;
+    int a = 3,b=8;
 	System.out.println("total : " + (a+b)); 
 	System.out.println("this is addes on 17 th August");
 }
