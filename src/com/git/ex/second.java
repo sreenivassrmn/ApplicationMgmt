a50050e318af0f17c5c6efbbf75c2d87054130d1
sreeni <sreenivasj23@gmail.com>
2019-01-06 13:50:00 +0530

    added changes

--- src/com/git/ex/second.java
+++ src/com/git/ex/second.java
@@ -2,7 +2,7 @@ package com.git.ex;
 class second {
 public void m1() {
     int a = 3,b=8;
-	System.out.println("total : " + (a+b)); 
+	System.out.println("total : " + (a+b));
 	System.out.println("this is addes on 17 th August");
 }
 }
